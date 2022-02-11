a2a1595dfe94742ed4e684abb6862d523dca20b6
Machuca Juan <machucajuangabriel@gmail.com>
2022-02-10 21:03:21 -0300

    black hat python 2nd edition

---
+++ venv3/scan.py
@@ -0,0 +1,16 @@
+from subprocess import Popen
+
+import argparse
+import os
+import scan
+
+def get_ip(machine_name):
+    pass
+
+class Scanner:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    scan = Scanner()
+    print('Hello')
